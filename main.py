b9754863466dc9d76655387bfde169e83a141ede
Jeremy Zehr <jeremy.e.zehr@gmail.com>
2023-03-19 14:07:25 +0100

    Use different threads

--- main.py
+++ main.py
@@ -2,16 +2,23 @@
 import time
 import math
 import random
+import threading
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageDraw, ImageTk
+from copy import deepcopy
 
 mapImg = Image.open('background.png')
 
-TARGET_FPS = 30
+# Note: executing a frame or cycle loop might take more than the target interval duration,
+# resulting in skipped loop(s). There's no real way around it for the frame loops,
+# but multiple cycle loops should be run if delay is too great
+
+TARGET_FPS = 20 # number of times the display is refreshed per second
+TARGET_CPS = 40 # number of cell cycles per second
 FERTILITY = 900 # number of cycles needed to recover from giving birth
 LIFE = 1200 # number of remaining cycles that cells start with
-NCELLS = 500 # Number of cells at the beginning
+NCELLS = 250 # Number of cells at the beginning
 MUTATION = 100 # Prevalence of mutation per letter (1/MUTATION)
 WIDTH = mapImg.width
 HEIGHT = mapImg.height
@@ -19,6 +26,8 @@ print("Size: {},{}".format(WIDTH,HEIGHT))
 BOTTOM_BAR = 40 # in px
 PIXELS = list(mapImg.getdata())
 DNA = "NSEWRLFB0" # N=North; S=South; E=East; W=West; R=Right (90deg rotation); L=Left (-90deg rotation); F=Forward; B=Back; 0=Do not move
+
+mouse_x, mouse_y = -1, -1
 
 root = tk.Tk()
 root.title('Pymuvie')
@@ -45,39 +54,73 @@ root.minsize(WIDTH, HEIGHT+BOTTOM_BAR)
 canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg='black')
 canvas.pack(anchor=tk.N, expand=True)
 
+info = tk.Label(root, text='0 fps, ncells: 0')
+info.pack(anchor=tk.W, expand=True)
+
+cell_info = tk.Label(root, text='Not currently hovering any cell')
+cell_info.pack(anchor=tk.E, expand=True)
+
+
 image_spots = Image.new(mode = "RGB", size = (WIDTH, HEIGHT), color = (0, 0, 0))
 draw_spots = ImageDraw.Draw(image_spots)
-image_render = Image.new(mode = "RGB", size = (WIDTH, HEIGHT), color = (0, 0, 0))
-draw_render = ImageDraw.Draw(image_render)
-tatras = ImageTk.PhotoImage(image_render)
+image_render = image_spots.copy()
+image_resized = image_render
+tatras = ImageTk.PhotoImage(image_resized)
 img_id = canvas.create_image(0, 0, anchor=tk.NW, image=tatras)
-# These two will be recreated at each cycle
-image_cells = None
-draw_cells = None
 
-def update_image(lbd):
+active_cell = None
+highlighted_cell = None
+
+updating_image = False
+def update_image(copy_cells,w,h):
   if root_open:
-    global image_cells, draw_cells
+    global updating_image
+    updating_image = True
     image_cells = Image.new(mode = "RGBA", size = (WIDTH, HEIGHT), color = (0, 0, 0, 0))
     draw_cells = ImageDraw.Draw(image_cells)
-    lbd()
-    image_render.paste(image_spots, (0,0))
+    global spots_to_refresh
+    for spot in spots_to_refresh:
+      spot.refresh(draw_spots)
+    spots_to_refresh = [s for s in spots_to_refresh if s.need_refresh>0]
+    for cell in copy_cells:
+      cell.refresh(draw_cells)
+
+    hovered = None
+    if (mouse_x>=0 and mouse_x<WIDTH and mouse_y>=0 and mouse_y<HEIGHT):
+      hovered = closestCellXY(mouse_x,mouse_y)
+    
+    if hovered != None:
+      hovered.highlight(draw_cells)
+    global highlighted_cell
+    if highlighted_cell:
+      if highlighted_cell.life>0:
+        highlighted_cell.highlight(draw_cells,"pink")
+      else:
+        highlighted_cell = None
+
+    global active_cell
+    active_cell = highlighted_cell or hovered
+    
+    image_render = image_spots.copy()
     image_render.paste(image_cells, (0,0), image_cells)
     # allow canvas resizing but enforce ratio
-    w, h = root.winfo_width(), root.winfo_height()
+    # w, h = root.winfo_width(), root.winfo_height()
     ratio_w, ratio_h = w/WIDTH, h/(HEIGHT+BOTTOM_BAR)
     ratio = min(ratio_w,ratio_h)
     new_w, new_h = round(WIDTH*ratio), round(HEIGHT*ratio)
+    global image_resized
     image_resized = image_render.resize((new_w,new_h))
-    canvas.config(width=new_w, height=new_h)
-    global tatras
-    tatras = ImageTk.PhotoImage(image_resized)
-    canvas.itemconfig(img_id,image=tatras)
-    root.update()
+    # global tatras
+    # tatras = ImageTk.PhotoImage(image_resized)
+    # canvas.config(width=new_w, height=new_h)
+    # canvas.itemconfig(img_id,image=tatras)
+    # root.update()
+    updating_image = False
 
 class Spot:
-  def refresh(self):
-    draw_spots.point([self.x,self.y], fill=(self.color[0],self.color[1],self.color[2]))
+  def refresh(self,d):
+    d.point([self.x,self.y], fill=(self.color[0],self.color[1],self.color[2]))
+    self.need_refresh -= 1
 
   def visit(self, cell):
     live_cells = [c for c in self.cells if c.life>=0]
@@ -100,13 +143,16 @@ class Spot:
     self.color[2] += 10
     if self.color[2] > 255:
       self.color[2] = 255
-    self.refresh()
+    if self.need_refresh == 0:
+      spots_to_refresh.append(self)
+    self.need_refresh += 1
 
   def __init__(self, x, y, color):
     self.x = x
     self.y = y
     self.color = [color[0],color[1],color[2]]
     self.cells = []
+    self.need_refresh = 0
     if self.color == [0,0,0]:
       self.wall = False
     else:
@@ -119,16 +165,16 @@ walls = [spot for spot in spots if spot.wall]
 nonWalls = [spot for spot in spots if not spot.wall]
 
 class Cell:
-  def refresh(self):
+  def refresh(self,d):
     color = (255,255,0)
     if self.female:
       color = (0,255,0)
     if self.life<=0:
       color = (255,0,0)
-    draw_cells.point([self.x,self.y], fill=color)
+    d.point([self.x,self.y], fill=color)
 
-  def highlight(self, color="purple"):
-    draw_cells.ellipse((self.x-5,self.y-5,self.x+5,self.y+5), fill=None, outline=color, width=2)
+  def highlight(self, d, color="purple"):
+    d.ellipse((self.x-5,self.y-5,self.x+5,self.y+5), fill=None, outline=color, width=2)
     
   def place(self,x,y):
     if (self.life<=0):
@@ -136,9 +182,6 @@ class Cell:
     old_spot = spotAtXY(self.x,self.y)
     if self in old_spot.cells:
       old_spot.cells.remove(self)
-      # old_spot.refresh(d)
-      # for cell in old_spot.cells:
-      #   cell.refresh(d)
     x = x%WIDTH
     y = y%HEIGHT
     spot = spotAtXY(x,y)
@@ -148,8 +191,7 @@ class Cell:
       self.y = y
       spot.visit(self)
     else:
-      old_spot.visit(self)  
-    self.refresh()
+      old_spot.visit(self)
 
   def __init__(self, x, y, dna="NSEWRLFBFLRWESN0", female=None):
     self.x = x
@@ -175,15 +217,10 @@ cells = [Cell(startingPoint.x,startingPoint.y) for startingPoint in startingPoin
 for i in range(0,NCELLS):
   cells[i].dna = cells[i].dna[0:i%len(cells[i].dna)]+cells[i].dna[i%len(cells[i].dna):len(cells[i].dna)]
 
-def init():
-  for wall in walls:
-    wall.refresh()
-  for cell in cells:
-    cell.place(cell.x,cell.y)
-    # cell.refresh(d)
-
-update_image(lambda:init())
-
+spots_to_refresh = [w for w in walls]
+for cell in cells:
+  cell.place(cell.x,cell.y)
+update_image(cells,root.winfo_width(),root.winfo_height())
 
 def closestCellXY(x,y,radius=10):
   spot = spotAtXY(x,y)
@@ -203,7 +240,6 @@ def closestCellXY(x,y,radius=10):
         return live_cells[0]
   return None
 
-mouse_x, mouse_y = -1, -1
 def clear_mouse(event=None):
   global mouse_x, mouse_y
   mouse_x, mouse_y = -1, -1
@@ -214,19 +250,19 @@ canvas.bind('<Motion>', motion)
 canvas.bind('<Leave>', clear_mouse)
 def click(event):
   cell = closestCellXY(mouse_x,mouse_y)
+  global highlighted_cell
   if (cell==None):
+    highlighted_cell = None
     return
-  if (cell in highlighted_cells):
-    highlighted_cells.remove(cell)
+  if (cell==highlighted_cell):
+    highlighted_cell = None
   else:
-    highlighted_cells.append(cell)
+    highlighted_cell = cell
 canvas.bind('<Button-1>', click)
 
-highlighted_cells = []
 def cycle():
   for cell in cells:
     if (cell.life<=0):
-      cell.refresh()
       continue
     cell.step = (cell.step+1) % len(cell.dna)
     cell.fertile_in = max(0,cell.fertile_in-1)
@@ -289,45 +325,56 @@ def cycle():
       cell.direction = "S"
     else:
       cell.direction = "0"
-
-  cell = None
-  if (mouse_x>=0 and mouse_x<WIDTH and mouse_y>=0 and mouse_y<HEIGHT):
-    cell = closestCellXY(mouse_x,mouse_y)
-  if cell == None:
-    cell_info.config(text='Not hovering cells currently')
-  else:
-    cell_info.config(text='Cell: {} hp, female? {}, fertile in {}, DNA is {}'.format(cell.life,cell.female,cell.fertile_in,cell.dna))
-    cell.highlight()
-
-  for cell in highlighted_cells:
-    cell.highlight("pink")
-
 # end cycle
 
-info = tk.Label(root, text='0 fps, ncells: 0')
-info.pack(anchor=tk.W, expand=True)
+fps = {'timestamp': 0, 'n': 0, 'c': 0, 'f': 0}
 
-cell_info = tk.Label(root, text='Not currently hovering any cell')
-cell_info.pack(anchor=tk.E, expand=True)
-
-fps = {'timestamp': 0, 'n': 0, 'l': 0}
-
-lastLoopTime = time.time()
+lastCycleTime = time.time()
+lastFrameTime = lastCycleTime
 while True:
   if not root_open:
     break
 
+  fps['n'] += 1
   currentTime = time.time()
+  
   if currentTime-fps['timestamp'] >= 1:
-    info.config(text='{} fps, {} lps, ncells: {} (alive {})'.format(fps['n'],fps['l'],len(cells),len([c for c in cells if c.life>0])))
+    info.config(text='{} fps, {} cps, {} lps, ncells: {} (alive {})'.format(fps['f'],fps['c'],fps['n'],len(cells),len([c for c in cells if c.life>0])))
     fps['n'] = 0
-    fps['l'] = 0
+    fps['f'] = 0
+    fps['c'] = 0
     fps['timestamp'] = currentTime
+  
+  cps_time_difference = currentTime-lastCycleTime
+  fps_time_difference = currentTime-lastFrameTime
+  
+  # Frame cycle (asynchronous - threaded)
+  if ((fps_time_difference) >= 1/TARGET_FPS and updating_image == False and root_open):
+    # get size now before window gets closed
+    root_w, root_h = root.winfo_width(),root.winfo_height()
 
-  fps['l'] += 1
-  dt = currentTime - lastLoopTime
-  if (dt >= 1/TARGET_FPS):
-    fps['n'] += 1
-    lastLoopTime = currentTime
+    tatras = ImageTk.PhotoImage(image_resized)
+    new_w, new_h = image_resized.size
+    canvas.config(width=new_w, height=new_h)
+    canvas.itemconfig(img_id,image=tatras)
+    if active_cell:
+      cell_info.config(text='Cell: {} hp, female? {}, fertile in {}, DNA is {}'.format(
+        active_cell.life,active_cell.female,active_cell.fertile_in,active_cell.dna)
+      )
+    else:
+      cell_info.config(text='No cell to display')
+    root.update()
 
-    update_image(lambda:cycle())
+    fps['f'] += 1
+    lastFrameTime = currentTime
+    # use deep copies of cells because the attributes of the original ones might be updated while the thread gets executed
+    copy_cells = [deepcopy(c) for c in cells]
+    ui_thread = threading.Thread(target=update_image, args=(copy_cells,root_w,root_h))
+    ui_thread.start()
+    
+  # Cell cycle (synchronous - main thread)
+  if (cps_time_difference >= 1/TARGET_CPS):
+    cycle()
+    fps['c'] += 1
+    lastCycleTime = currentTime
+    
